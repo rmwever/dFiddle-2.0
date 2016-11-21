b10c3c76efc543bc7ef0601e2ce6cac3c0df2abe
Ryan Wever <rmw9p@virginia.edu>
2016-11-21 17:05:18 -0500

    Update index.js

--- app/ko/helloWorld2/index.js
+++ app/ko/helloWorld2/index.js
@@ -1,21 +1,75 @@
 ﻿define(['knockout'], function (ko) {
-    var firstName = ko.observable("updateMe");
-    var lastName = ko.observable("meTooPlease");
+    
+        ko.components.register('dashboard-grid', {
+            viewModel: {
+                createViewModel: function (controller, componentInfo) {
+                    var ViewModel = function (controller, componentInfo) {
+                        var grid = null;
 
-    var fullName = ko.computed(function () {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return firstName() + " " + lastName();
-    });
+                        this.widgets = controller.widgets;
 
-    return  {
-        firstName: firstName,
-        lastName: lastName,
-        fullName: fullName,
-        activate: activate
+                        this.afterAddWidget = function (items) {
+                            if (grid == null) {
+                                grid = $(componentInfo.element).find('.grid-stack').gridstack({
+                                    auto: false
+                                }).data('gridstack');
+                            }
+
+                            var item = _.find(items, function (i) { return i.nodeType == 1 });
+                            grid.addWidget(item);
+                            ko.utils.domNodeDisposal.addDisposeCallback(item, function () {
+                                grid.removeWidget(item);
+                            });
+                        };
+                    };
+
+                    return new ViewModel(controller, componentInfo);
+                }
+            },
+            template:
+                [
+                    '<div class="grid-stack" data-bind="foreach: {data: widgets, afterRender: afterAddWidget}">',
+                    '   <div class="grid-stack-item" data-bind="attr: {\'data-gs-x\': $data.x, \'data-gs-y\': $data.y, \'data-gs-width\': $data.width, \'data-gs-height\': $data.height, \'data-gs-auto-position\': $data.auto_position}">',
+                    '       <div class="grid-stack-item-content"><button data-bind="click: $root.deleteWidget">Delete me</button></div>',
+                    '   </div>',
+                    '</div> '
+                ].join('')
+        });
+
+        
+            var Controller = function (widgets) {
+                var self = this;
+
+                this.widgets = ko.observableArray(widgets);
+
+                this.addNewWidget = function () {
+                    this.widgets.push({
+                        x: 0,
+                        y: 0,
+                        width: Math.floor(1 + 3 * Math.random()),
+                        height: Math.floor(1 + 3 * Math.random()),
+                        auto_position: true
+                    });
+                    return false;
+                };
+
+                this.deleteWidget = function (item) {
+                    self.widgets.remove(item);
+                    return false;
+                };
+            };
+        
+    
+     
+     var vm = {
+         widgets: [
+                {x: 0, y: 0, width: 2, height: 2},
+                {x: 2, y: 0, width: 4, height: 2},
+                {x: 6, y: 0, width: 2, height: 4},
+                {x: 1, y: 2, width: 4, height: 2}
+            ],
+    controller: new Controller(vm.widgets)
     };
-
-    function activate(){
-       firstName("Planet");
-       lastName("Earth");
-   }
+     
+     return vm;
 });
